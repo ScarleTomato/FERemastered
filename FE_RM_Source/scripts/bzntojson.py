c31c8fcea396880d90a5e7d1410fc3154ee305a3
Mike <shortymack15@hotmail.com>
2022-12-01 22:26:26 -0600

    multi array

--- FE_RM_Source/scripts/bzntojson.py
+++ FE_RM_Source/scripts/bzntojson.py
@@ -33,24 +33,31 @@ def flatten(obj):
     return ret
   return obj
 
-class BZNObject(dict):
+class BZNObject(list):
 
   def fromLines(self, lines):
     top = self
+    # everything with a [#] is a list of dictionaries
+    # if the dictionary has a single unnamed value, name it '_'
+    # for ease, [GameObject] is also a list of one dictionary
+    top.append({})
+    # Keep track of which element we're adding to
+    listndx = 0
+    # the parent is the list that i'm adding into
     parents = [top]
     name = ''
-    tab = -1
     for line in lines:
-      # get the current parent
-      parent = parents[-1]
+      # get the current parent (the last one in the parents list)
+      parent:list = parents[-1]
       # if line like 'key = value'
       if re.match(r'^\w+ = \w+$', line):
         if not parent is top:
-          # these lines always describe a top level element
+          # these lines always describe a top level element, so move to the top
           parent=top
           parents = [top]
+          listndx = 0
         spl = line.split(' = ')
-        top[spl[0]] = spl[1][:-1]
+        top[0][spl[0]] = spl[1][:-1]
       # if line 'key =\r\n'
       if re.match(r'^\s*\S+ (\[\d+\] ){0,1}=$', line):
         indent = tabs(line)
@@ -58,23 +65,26 @@ class BZNObject(dict):
         if indent == 0:
           parent=top
           parents = [top]
+          listndx = 0
         # if i've found a key with the same indent as the previous parent. i'm done with that parent. move up
-        elif indent == parent['indent']:
+        elif indent == parent[0]['indent']:
           parents.pop()
           parent = parents[-1]
+          listndx = 0
         # get the name of this key
         name = re.findall(r'\S+', line)[0]
-        # add this key to the current parent
-        parent[name] = [{'indent':indent}]
+        # if this key already exists in the last element
+        if parent[-1].get(name):
+          # make a new element in this parent
+          parent.append({})
+        # add this key to the last element in current parent
+        parent[-1][name] = [{'indent':indent}]
         # and then set this as the new parent
-        parents.append(parent[name][0])
-        # if less indented, move up one parent
-        # if tabs(line) < tab:
-        #   parents.pop()
+        parents.append(parent[-1][name])
       # if line is 'value'
       if re.match(r'^[\w\.\-\+]+$', line):
         # done with this parent, pop it off
-        parents.pop()['_'] =  line[:-1]
+        parents.pop()[-1]['_'] = line[:-1]
 
       # if re.search('\S', a).start() > tab:
 
@@ -86,9 +96,7 @@ class BZNObject(dict):
 class BZN:
   header = []
   footer = []
-  objectlines = []
   objects = []
-  pathlines = []
   paths = []
 
   def readHeader(self, f):
@@ -107,37 +115,35 @@ class BZN:
       line = f.readline()
 
   def readObjects(self, f):
+    objectlines = []
     skip(f, 2)
     line:str = f.readline()
     while not line.startswith('groupTargets'):
       if line.startswith('[GameObject]'):
         object = []
-        self.objectlines.append(object)
+        objectlines.append(object)
       else:
         object.append(line)
       line = f.readline()
     # backup to the beginning of the last line
     f.seek(f.tell() - len(line) - 1)
+    for lines in objectlines:
+      self.objects.append(BZNObject().fromLines(lines))
 
   def readPaths(self, f):
+    pathlines = []
     skip(f, 7)
     line:str = f.readline()
     while not line.startswith('hasEntered'):
       if line.startswith('name = AiPath'):
         path = []
-        self.pathlines.append(path)
+        pathlines.append(path)
       else:
         path.append(line)
       line = f.readline()
     # backup to the beginning of the last line
     f.seek(f.tell() - len(line) - 1)
-  
-  def parseObjects(self):
-    for lines in self.objectlines:
-      self.objects.append(BZNObject().fromLines(lines))
-  
-  def parsePaths(self):
-    for lines in self.pathlines:
+    for lines in pathlines:
       self.paths.append(BZNObject().fromLines(lines))
 
   def fromFile(self, fn):
@@ -148,14 +154,12 @@ class BZN:
       self.dllName = f.readline()
       self.readPaths(f)
       self.readFooter(f)
-    self.parseObjects()
-    self.parsePaths()
     # self.parsePaths()
 
-fn = r'C:\Users\Mike\Documents\My Games\Battlezone Combat Commander\FE\addon\missions\Multiplayer\test\test.bzn'
+fn = r'C:\Users\Mike\Documents\My Games\Battlezone Combat Commander\FE\addon\missions\Multiplayer\test\toparse.bzn'
 bzn = BZN()
 bzn.fromFile(fn)
 with open(r'C:\Users\Mike\Documents\My Games\Battlezone Combat Commander\FE\addon\missions\Multiplayer\test\test.json', 'w') as f:
-  f.write(json.dumps(bzn.objects, indent=2))
+  f.write(json.dumps(bzn.paths, indent=2))
 with open(r'C:\Users\Mike\Documents\My Games\Battlezone Combat Commander\FE\addon\missions\Multiplayer\test\flat.json', 'w') as f:
-  f.write(json.dumps(flatten(bzn.objects), indent=2))
+  f.write(json.dumps(flatten(bzn.paths), indent=2))
